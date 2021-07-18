edb0164cfdecd854f221ac9a8f7fa50fb88ad779
Mateusz Adamowski <m.adamowski@alx.pl>
2021-07-18 11:44:47 +0200

    brakująca flaga z

--- cw01/cw4.sh
+++ cw01/cw4.sh
@@ -2,5 +2,5 @@
 
 echo "Pakuję dokumentację findutils do backup.tar.gz"
 
-tar -cf backup-$(date +%F).tar.gz -C /usr/share/doc  findutils-4.5.11
+tar -zcf backup-$(date +%F).tar.gz -C /usr/share/doc  findutils-4.5.11
 
