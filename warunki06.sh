6bb20eb53b577ffcd629ed3963c86fc2936b3229
Mateusz Adamowski <m.adamowski@alx.pl>
2021-07-31 09:19:29 +0200

    możliwe rozwiązanie

--- warunki06.sh
+++ warunki06.sh
@@ -4,7 +4,12 @@ read -p "Podaj liczbę z przedziału 10-100: " n
 
 if [ "$n" -ge 10 ]
 then
-	echo "Liczba $n jest OK"
+	if [ "$n" -le 100 ]
+	then
+		echo "Liczba $n jest OK"
+	else
+		echo "Za duża"
+	fi
 else
 	echo "Źle"
 fi
