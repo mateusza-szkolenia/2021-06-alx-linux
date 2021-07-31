1dd3ef7529c4974c26652696a8e57df94d04e10b
Mateusz Adamowski <m.adamowski@alx.pl>
2021-07-31 14:10:55 +0200

    petla for

---
+++ petle01.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# (w niektórych językach to jest foreach)
+
+for miesiac in Styczen Luty Marzec Kwiecien Maj Czerwiec Lipiec Sierpien Wrzesien Pazdziernik Listopad Grudzien
+do
+	echo "Mamy teraz miesiąc: $miesiac"
+	echo "Jest miło"
+	echo ""
+done
+
