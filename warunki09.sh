93f53a20c559a496563dea4eb55a290ce49d9512
Mateusz Adamowski <m.adamowski@alx.pl>
2021-07-31 09:46:41 +0200

    kopia

---
+++ warunki09.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# napisać skrypt, który sprawdzi czy istnieje katalog "alamakota"
+# jeśli nie, to go utworzy
+
+if ! [ -d "alamakota" ]
+then
+	mkdir "alamakota"
+else
+	echo "Katalog istnieje, nie robię nic"
+fi
+
