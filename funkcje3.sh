2218648e0ccaad96c1c8902c6e42d00cac01d6cd
Mateusz Adamowski <m.adamowski@alx.pl>
2021-07-31 16:51:04 +0200

    błąd - nadpisanie zmiennej

---
+++ funkcje3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+powitaj () {
+	echo -n "$1: "
+	for x in {0001..0007}
+	do
+		echo -n "$x "
+	done
+	echo
+
+}
+
+
+
+for x in Mateusz Marcin Michal Andrzej Zygmunt
+do
+	powitaj "$x"
+	echo "$x zostal powitany"
+done
+
