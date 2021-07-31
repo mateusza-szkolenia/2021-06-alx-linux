4d1ae41a7e75acb7991ede9d156fb9c27425ba95
Mateusz Adamowski <m.adamowski@alx.pl>
2021-07-31 09:21:11 +0200

    łączenie warunków operatorem &&

--- warunki06.sh
+++ warunki06.sh
@@ -2,14 +2,9 @@
 
 read -p "Podaj liczbę z przedziału 10-100: " n
 
-if [ "$n" -ge 10 ]
+if [ "$n" -ge 10 ] && [ "$n" -le 100 ]
 then
-	if [ "$n" -le 100 ]
-	then
-		echo "Liczba $n jest OK"
-	else
-		echo "Za duża"
-	fi
+	echo "Liczba $n jest OK"
 else
 	echo "Źle"
 fi
